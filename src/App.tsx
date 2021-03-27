8b0621bbc11290e8879d38a056ce44e99d1308da
Ricardo Melhado <rcdmelhado@hotmail.com>
2021-03-27 01:42:22 -0300

    adding main, products, products create and products edit pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css';
 import Products from "./admin/Products";
+import ProductsCreate from "./admin/ProductsCreate";
+import ProductsEdit from "./admin/ProductsEdit";
 import Main from "./main/Main";
 import { BrowserRouter, Route } from "react-router-dom";
 
@@ -8,7 +10,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Route path='/' exact component={Main} />
-        <Route path='/admin/products' component={Products} />
+        <Route path='/admin/products' exact component={Products} />
+        <Route path='/admin/products/create' exact component={ProductsCreate} />
+        <Route path='/admin/products/:id/edit' exact component={ProductsEdit} />
       </BrowserRouter>
     </div>
   );
--- src/admin/Products.tsx
+++ src/admin/Products.tsx
@@ -1,20 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import { Product } from '../interfaces/product';
-import Wrapper from './Wrapper';
+import Wrapper from "./Wrapper";
+import { Product } from "../interfaces/product";
+import { Link } from "react-router-dom";
 
 const Products = () => {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+
     useEffect(() => {
         (
             async () => {
                 const response = await fetch('http://localhost:8000/api/products');
+
                 const data = await response.json();
+
                 setProducts(data);
             }
         )();
-    }, [])
+    }, []);
+
+    const del = async (id: number) => {
+        if (window.confirm('Are you sure you want to delete this product?')) {
+            await fetch(`http://localhost:8000/api/products/${id}`, {
+                method: 'DELETE'
+            });
+
+            setProducts(products.filter((p: Product) => p.id !== id));
+        }
+    }
+
     return (
         <Wrapper>
+            <div className="pt-3 pb-2 mb-3 border-bottom">
+                <div className="btn-toolbar mb-2 mb-md-0">
+                    <Link to='/admin/products/create' className="btn btn-sm btn-outline-secondary">Add</Link>
+                </div>
+            </div>
+
             <div className="table-responsive">
                 <table className="table table-striped table-sm">
                     <thead>
@@ -32,10 +53,18 @@ const Products = () => {
                                 return (
                                     <tr key={p.id}>
                                         <td>{p.id}</td>
-                                        <td><img src={p.image} height="180" /></td>
+                                        <td><img src={p.image} height="180"/></td>
                                         <td>{p.title}</td>
                                         <td>{p.likes}</td>
-                                        <td></td>
+                                        <td>
+                                            <div className="btn-group mr-2">
+                                                <Link to={`/admin/products/${p.id}/edit`}
+                                                    className="btn btn-sm btn-outline-secondary">Edit</Link>
+                                                <a href="#" className="btn btn-sm btn-outline-secondary"
+                                                    onClick={() => del(p.id)}
+                                                >Delete</a>
+                                            </div>
+                                        </td>
                                     </tr>
                                 )
                             })}
